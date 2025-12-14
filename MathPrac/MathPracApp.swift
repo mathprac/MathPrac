e1cf2b2603803111b77917c1a42b55a1d761d65a
shashwath <shashwath.manjunath@gmail.com>
2025-12-14 11:25:11 -0800

    First version of MathPrac

--- MathPrac/MathPracApp.swift
+++ MathPrac/MathPracApp.swift
@@ -1,9 +1,12 @@
+// MathPracticePro - iOS App
 //
-//  MathPracApp.swift
-//  MathPrac
-//
-//  Created by Shashwath Manjunath on 11/30/25.
-//
+// SETUP INSTRUCTIONS:
+// 1. Create a new Xcode project: File > New > Project > iOS > App
+// 2. Product Name: MathPracticePro
+// 3. Interface: SwiftUI, Language: Swift
+// 4. Copy all these files into your project
+// 5. Update the API_BASE_URL in APIService.swift with your deployed Replit URL
+// 6. Build and run on your device or simulator
 
 import SwiftUI
 
